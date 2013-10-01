259d77c1277cf8d2fc704cc9171de358a47e6202
Matthäus Zloch <matthaeus.zloch@gmail.com>
2013-10-01 13:59:16 +0200

    java.sql.Date -> java.util.Date

--- src/main/java/org/gesis/discovery/persistence/relational/PeriodOfTimeDAOHibernate.java
+++ src/main/java/org/gesis/discovery/persistence/relational/PeriodOfTimeDAOHibernate.java
@@ -1,16 +1,29 @@
 package org.gesis.discovery.persistence.relational;
 
+import java.util.Date;
+
 import org.gesis.dcterms.PeriodOfTime;
 import org.gesis.discovery.persistence.PeriodOfTimeDAO;
 import org.gesis.persistence.relational.GenericHibernateDAO;
 import org.springframework.orm.hibernate3.HibernateTemplate;
 
+/**
+ * @author matthaeus
+ * 
+ */
 public class PeriodOfTimeDAOHibernate extends GenericHibernateDAO<PeriodOfTime> implements PeriodOfTimeDAO
 {
 
 	public PeriodOfTimeDAOHibernate( final HibernateTemplate hibernateTemplate )
 	{
 		super( hibernateTemplate );
+	}
+
+	@Override
+	public PeriodOfTime getByStartDate( final Date date )
+	{
+		// TODO ZL implement method
+		return null;
 	}
 
 }
