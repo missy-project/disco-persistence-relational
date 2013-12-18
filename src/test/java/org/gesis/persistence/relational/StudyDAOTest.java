555e511e7a6eb6b716f8635f72be478bd0203a54
Matthaeus Zloch <matthaeus.zloch@gmail.com>
2013-12-18 14:46:02 +0100

    refactoring: GenericHibernateDAOTest -> StudyDAOTest

--- src/test/java/org/gesis/persistence/relational/StudyDAOTest.java
+++ src/test/java/org/gesis/persistence/relational/StudyDAOTest.java
@@ -21,21 +21,21 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith( SpringJUnit4ClassRunner.class )
-@ContextConfiguration( locations = { "classpath:context-persistence-relational-test.xml" } )
+@ContextConfiguration( locations = { "classpath:context-persistence-test-inMemoryDB.xml" } )
 @TransactionConfiguration
-public class GenericHibernateDAOTest
+public class StudyDAOTest
 {
 	@Autowired
 	protected PersistenceStrategy persistenceStrategy;
 
-	private StudyDAO dao;
+	private StudyDAO studyDAO;
 	private Study study;
 
 	@Before
 	public void init()
 	{
-		dao = persistenceStrategy.getStudyDAO();
-		assertNotNull( dao );
+		studyDAO = persistenceStrategy.getStudyDAO();
+		assertNotNull( studyDAO );
 
 		study = new Study();
 		study.setTitle( LangString.createUKLangString( "new uk title" ) );
@@ -47,21 +47,21 @@ public class GenericHibernateDAOTest
 
 		study.addVariable( variable );
 
-		dao.persist( study );
+		studyDAO.persist( study );
 	}
 
 	@After
 	public void deleteStudies()
 	{
 		if ( study != null )
-			dao.delete( study );
+			studyDAO.delete( study );
 	}
 
 	@Test
 	@Transactional
 	public void getAll()
 	{
-		final List<Study> all = dao.getAll();
+		final List<Study> all = studyDAO.getAll();
 		assertNotNull( all );
 		assertEquals( 1, all.size() );
 
@@ -74,7 +74,7 @@ public class GenericHibernateDAOTest
 	@Transactional
 	public void getById()
 	{
-		final Study persistedStudy = dao.getById( study.getId(), false );
+		final Study persistedStudy = studyDAO.getById( study.getId(), false );
 		assertNotNull( persistedStudy );
 		assertEquals( study.getTitle().getEn(), persistedStudy.getTitle().getEn() );
 	}
@@ -83,7 +83,7 @@ public class GenericHibernateDAOTest
 	@Transactional
 	public void getByURN()
 	{
-		final Study persistedStudy = dao.getByURN( Study.class, "agencyId:study:version" );
+		final Study persistedStudy = studyDAO.getByURN( Study.class, "agencyId:study:version" );
 		assertNotNull( persistedStudy );
 		assertEquals( "new uk title", persistedStudy.getTitle().getEn() );
 		assertEquals( "new de abstract", persistedStudy.getAbstract().getDe() );
@@ -92,7 +92,7 @@ public class GenericHibernateDAOTest
 	@Test
 	public void getVariableByURN()
 	{
-		final Variable persistedVariable = dao.getByURN( Variable.class, "agencyId:variable:version" );
+		final Variable persistedVariable = studyDAO.getByURN( Variable.class, "agencyId:variable:version" );
 		assertNotNull( persistedVariable );
 	}
 }
