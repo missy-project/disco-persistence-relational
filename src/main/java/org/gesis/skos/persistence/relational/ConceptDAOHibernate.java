7fb66d9fe4aa953bf98e8c5da8305952ec6d5e76
Matthaeus Zloch <matthaeus.zloch@gmail.com>
2014-03-24 10:54:13 +0100

    extends GenericResourceHibernateDAO

--- src/main/java/org/gesis/skos/persistence/relational/ConceptDAOHibernate.java
+++ src/main/java/org/gesis/skos/persistence/relational/ConceptDAOHibernate.java
@@ -2,7 +2,7 @@ package org.gesis.skos.persistence.relational;
 
 import java.util.List;
 
-import org.gesis.persistence.relational.GenericHibernateDAO;
+import org.gesis.persistence.relational.GenericResourceHibernateDAO;
 import org.gesis.skos.Concept;
 import org.gesis.skos.persistence.ConceptDAO;
 import org.springframework.orm.hibernate3.HibernateTemplate;
@@ -13,7 +13,7 @@ import org.springframework.orm.hibernate3.HibernateTemplate;
  * @author matthaeus
  * 
  */
-public class ConceptDAOHibernate extends GenericHibernateDAO<Concept> implements ConceptDAO
+public class ConceptDAOHibernate extends GenericResourceHibernateDAO<Concept> implements ConceptDAO
 {
 
 	public ConceptDAOHibernate(final HibernateTemplate hibernateTemplate)
