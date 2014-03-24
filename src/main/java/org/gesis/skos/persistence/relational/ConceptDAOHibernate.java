41131d0423e122f98627b8296cb46688e35aefff
Matthaeus Zloch <matthaeus.zloch@gmail.com>
2014-03-24 10:56:43 +0100

    implemented getByNotation()

--- src/main/java/org/gesis/skos/persistence/relational/ConceptDAOHibernate.java
+++ src/main/java/org/gesis/skos/persistence/relational/ConceptDAOHibernate.java
@@ -1,11 +1,13 @@
 package org.gesis.skos.persistence.relational;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.gesis.persistence.relational.GenericResourceHibernateDAO;
 import org.gesis.skos.Concept;
 import org.gesis.skos.persistence.ConceptDAO;
 import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.util.StringUtils;
 
 /**
  * Hibernate implementation for Concept.
@@ -24,8 +26,16 @@ public class ConceptDAOHibernate extends GenericResourceHibernateDAO<Concept> im
 	@Override
 	public List<Concept> getByNotation( final String notation )
 	{
-		// TODO Auto-generated method stub
-		return null;
+		if ( StringUtils.isEmpty( notation ) )
+			return Collections.emptyList();
+
+		@SuppressWarnings( "unchecked" )
+		List<Concept> concepts = getHibernateTemplate().find( "FROM " + getPersistenceClass().getName() + " c WHERE c.notation = ?", notation );
+
+		if ( concepts == null )
+			return Collections.emptyList();
+
+		return concepts;
 	}
 
 }
