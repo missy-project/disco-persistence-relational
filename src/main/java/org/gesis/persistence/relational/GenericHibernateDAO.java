10480591d83cd3b1854a6ed386d43af1d3bc37e3
Matthaeus Zloch <matthaeus.zloch@gmail.com>
2014-12-17 14:22:26 +0100

    GenericHibernateDAO -> added default constructor; made hibernateTEmplate and sessionFactory @Autowired fields; added getter and setter for them; updated doc

--- src/main/java/org/gesis/persistence/relational/GenericHibernateDAO.java
+++ src/main/java/org/gesis/persistence/relational/GenericHibernateDAO.java
@@ -12,6 +12,7 @@ import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.Example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,11 +33,41 @@ public abstract class GenericHibernateDAO<T> implements GenericDAO<T>
 	/**
 	 * Is going to be injected.
 	 */
-	private final SessionFactory sessionFactory;
-	private final HibernateTemplate hibernateTemplate;
+	@Autowired
+	private SessionFactory sessionFactory = null;
+
+	/**
+	 * Is going to be injected.
+	 */
+	@Autowired( required = false )
+	private HibernateTemplate hibernateTemplate = null;
 
 	private Class<T> persistenceClass;
 
+	/**
+	 * Default constructor. Necessary in cases when this DAO-object is being
+	 * created by a spring-context.
+	 */
+	@SuppressWarnings( "unchecked" )
+	public GenericHibernateDAO()
+	{
+		if ( getClass().getGenericSuperclass() instanceof ParameterizedType )
+			this.persistenceClass = (Class<T>) ( (ParameterizedType) getClass().getGenericSuperclass() ).getActualTypeArguments()[0];
+		// in case we have more than one parent
+		// TODO ZL rewrite. not the best solution, because not generic
+		else if ( getClass().getSuperclass().getGenericSuperclass() instanceof ParameterizedType )
+			this.persistenceClass = (Class<T>) ( (ParameterizedType) getClass().getSuperclass().getGenericSuperclass() ).getActualTypeArguments()[0];
+		else
+			this.persistenceClass = null;
+	}
+
+	/**
+	 * Usually the specific DAO-class is instantiated by the spring-context. All
+	 * required objects are then injected by spring. Use this constructor in
+	 * case you want to instantiate the DAO-class manually.
+	 * 
+	 * @param hibernateTemplate
+	 */
 	@SuppressWarnings( "unchecked" )
 	public GenericHibernateDAO( final HibernateTemplate hibernateTemplate )
 	{
@@ -47,9 +78,12 @@ public abstract class GenericHibernateDAO<T> implements GenericDAO<T>
 		else
 			this.persistenceClass = null;
 
+		// manually instantiated
 		this.hibernateTemplate = hibernateTemplate;
 		this.sessionFactory = hibernateTemplate.getSessionFactory();
 	}
+
+	// getter / setter
 
 	/**
 	 * The hibernate template used by this class.
@@ -59,6 +93,26 @@ public abstract class GenericHibernateDAO<T> implements GenericDAO<T>
 	public HibernateTemplate getHibernateTemplate()
 	{
 		return this.hibernateTemplate;
+	}
+
+	public void setHibernateTemplate( final HibernateTemplate hibernateTemplate )
+	{
+		this.hibernateTemplate = hibernateTemplate;
+	}
+
+	/**
+	 * The session factory used by this class.
+	 * 
+	 * @return
+	 */
+	public SessionFactory getSessionFactory()
+	{
+		return sessionFactory;
+	}
+
+	public void setSessionFactory( final SessionFactory sessionFactory )
+	{
+		this.sessionFactory = sessionFactory;
 	}
 
 	/**
@@ -83,12 +137,12 @@ public abstract class GenericHibernateDAO<T> implements GenericDAO<T>
 		return this.persistenceClass;
 	}
 
+	// DAO methods
+
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see
-	 * org.gesis.ddi.persistence.dataAccess.GenericDAO#getById(java.lang.String,
-	 * boolean)
+	 * @see org.gesis.persistence.GenericDAO#getById(java.lang.String)
 	 */
 	@Override
 	@Transactional
@@ -101,7 +155,7 @@ public abstract class GenericHibernateDAO<T> implements GenericDAO<T>
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.gesis.ddi.persistence.dataAccess.GenericDAO#getAll()
+	 * @see org.gesis.persistence.GenericDAO#getAll()
 	 */
 	@Override
 	@Transactional
@@ -118,9 +172,8 @@ public abstract class GenericHibernateDAO<T> implements GenericDAO<T>
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see
-	 * org.gesis.ddi.persistence.dataAccess.GenericDAO#getByExample(java.lang
-	 * .Object, java.lang.String[])
+	 * @see org.gesis.persistence.GenericDAO#getByExample(java.lang.Object,
+	 * java.lang.String[])
 	 */
 	@Override
 	@Transactional
@@ -147,8 +200,7 @@ public abstract class GenericHibernateDAO<T> implements GenericDAO<T>
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see
-	 * org.gesis.ddi.persistence.dataAccess.GenericDAO#persist(java.lang.Object)
+	 * @see org.gesis.persistence.GenericDAO#persist(java.lang.Object)
 	 */
 	@Override
 	@Transactional
@@ -164,8 +216,7 @@ public abstract class GenericHibernateDAO<T> implements GenericDAO<T>
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see
-	 * org.gesis.ddi.persistence.dataAccess.GenericDAO#delete(java.lang.Object)
+	 * @see org.gesis.persistence.GenericDAO#delete(java.lang.Object)
 	 */
 	@Override
 	@Transactional
