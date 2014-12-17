6b7a482b37580475b4ea63962fd79a7acb002c0e
Matthaeus Zloch <matthaeus.zloch@gmail.com>
2014-12-17 14:20:43 +0100

    PeriodOfTimeDAO -> added default constructor; removed method getByStartDate()

--- src/main/java/org/gesis/discovery/persistence/relational/PeriodOfTimeDAOHibernate.java
+++ src/main/java/org/gesis/discovery/persistence/relational/PeriodOfTimeDAOHibernate.java
@@ -1,7 +1,5 @@
 package org.gesis.discovery.persistence.relational;
 
-import java.util.Date;
-
 import org.gesis.dcterms.PeriodOfTime;
 import org.gesis.discovery.persistence.PeriodOfTimeDAO;
 import org.gesis.persistence.relational.GenericResourceHibernateDAO;
@@ -14,16 +12,14 @@ import org.springframework.orm.hibernate3.HibernateTemplate;
 public class PeriodOfTimeDAOHibernate extends GenericResourceHibernateDAO<PeriodOfTime> implements PeriodOfTimeDAO
 {
 
+	public PeriodOfTimeDAOHibernate()
+	{
+		super();
+	}
+
 	public PeriodOfTimeDAOHibernate( final HibernateTemplate hibernateTemplate )
 	{
 		super( hibernateTemplate );
-	}
-
-	@Override
-	public PeriodOfTime getByStartDate( final Date date )
-	{
-		// TODO ZL implement method
-		return null;
 	}
 
 }
